f73cdacd9fb729dd6e11bea141c1924cdb7ef7a9
kasunprabashwara <kasunprabashwara24@gmail.com>
2024-11-14 19:20:02 +0530

    added new data

--- data.py
+++ data.py
@@ -2,6 +2,7 @@
 
 import os
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import TextLoader
 
 def get_data(docs_folder="docs"):
     documents = []
@@ -10,5 +11,10 @@ def get_data(docs_folder="docs"):
             print(f"Loading document: {filename}")
             file_path = os.path.join(docs_folder, filename)
             loader = PyPDFLoader(file_path)
+            documents.extend(loader.load_and_split())
+        if filename.endswith(".txt"):
+            print(f"Loading document: {filename}")
+            file_path = os.path.join(docs_folder, filename)
+            loader = TextLoader(file_path)
             documents.extend(loader.load_and_split())
     return documents
